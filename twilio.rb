884ba32da8215120ec0a3b7f88f0f602a50f052b
Dylan P. Hancock <84444837+Dylan-P-H@users.noreply.github.com>
2021-07-31 15:44:11 +0100

    Update twilio.rb

--- twilio.rb
+++ twilio.rb
@@ -2,8 +2,8 @@
 require 'rubygems'
 require 'twilio-ruby'
 
-account_sid = 'AC1ba5399ec4a3eb888a67d0e118bdb52e'
-auth_token = 'f03554c9322686a44a28e91051b3327e'
+account_sid = ''
+auth_token = ''
 @client = Twilio::REST::Client.new(account_sid, auth_token)
 
 message = @client.messages
